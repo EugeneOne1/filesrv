9cee493a5d9be0bfb16d485fe414276ec9c6988c
Eugene Burkov <E.Burkov@AdGuard.COM>
2023-05-27 17:50:21 +0300

    dirs: imp path parts and title

--- internal/dirs/read.go
+++ internal/dirs/read.go
@@ -4,14 +4,35 @@ import (
 	"fmt"
 	"io/fs"
 	"net/http"
+	"net/url"
 	"strings"
-
-	"filesrv/internal/errors"
 
 	"golang.org/x/exp/slices"
 )
 
-var ErrNotDir = errors.Error("not a directory")
+// read reads the directory named by path and returns a list of directory
+// entries.
+func (h *Dirs) read(path string) (entries []fs.FileInfo, err error) {
+	f, err := h.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, err
+	} else if !fi.IsDir() {
+		return nil, fs.ErrNotExist
+	}
+
+	entries, err = f.Readdir(-1)
+	if err != nil {
+		return nil, err
+	}
+
+	return entries, nil
+}
 
 // handleDir reads the directory and marshals the entries via the template.
 func (h *Dirs) handleDir(w http.ResponseWriter, r *http.Request) (err error) {
@@ -47,62 +68,49 @@ type pathPart struct {
 	Path string
 }
 
-func renderPage(w http.ResponseWriter, r *http.Request, entries []fs.FileInfo) (err error) {
-	dirs := strings.Split(strings.TrimSuffix(r.URL.Path, "/"), "/")
-	parts := make([]pathPart, 0, len(dirs))
+func pathParts(p string) (current string, parts []pathPart) {
+	dirs := strings.Split(strings.TrimSuffix(p, "/"), "/")
+	parts = make([]pathPart, 0, len(dirs))
+	parts = append(parts, pathPart{
+		Dir:  "",
+		Path: "/",
+	})
+	current = "/"
 
-	current := "/"
-	for i := range dirs {
+	for i := range dirs[1:] {
 		parts = append(parts, pathPart{
-			Dir:  dirs[i],
-			Path: strings.Join(dirs[:i+1], "/") + "/",
+			Dir:  dirs[i+1],
+			Path: strings.Join(dirs[:i+2], "/") + "/",
 		})
-		current = parts[i].Dir
+		current = parts[i+1].Dir
 	}
 
 	for i, j := 0, len(parts)-1; i < j; i, j = i+1, j-1 {
 		parts[i], parts[j] = parts[j], parts[i]
 	}
 
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	err = t.Lookup("dir.gohtml").Execute(w, struct {
+	return current, parts
+}
+
+func renderPage(w http.ResponseWriter, r *http.Request, entries []fs.FileInfo) (err error) {
+	templData := struct {
 		CurrentDir string
 		PathParts  []pathPart
 		Path       string
+		Params     url.Values
 		Entries    []fs.FileInfo
 	}{
-		CurrentDir: current,
-		PathParts:  parts,
-		Path:       r.URL.Path,
-		Entries:    entries,
-	})
+		Path:    r.URL.Path,
+		Entries: entries,
+	}
+
+	templData.CurrentDir, templData.PathParts = pathParts(r.URL.Path)
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err = t.Lookup("dir.gohtml").Execute(w, templData)
 	if err != nil {
 		return fmt.Errorf("executing template: %w", err)
 	}
 
 	return nil
-}
-
-// read reads the directory named by path and returns a list of directory
-// entries.
-func (h *Dirs) read(path string) (entries []fs.FileInfo, err error) {
-	f, err := h.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	fi, err := f.Stat()
-	if err != nil {
-		return nil, err
-	} else if !fi.IsDir() {
-		return nil, fs.ErrNotExist
-	}
-
-	entries, err = f.Readdir(-1)
-	if err != nil {
-		return nil, err
-	}
-
-	return entries, nil
 }
