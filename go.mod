c2d2669cc7129fe84c4729b8ecde5807517eb01d
Eugene Burkov <E.Burkov@AdGuard.COM>
2023-06-06 12:33:37 +0300

    add qr

--- go.mod
+++ go.mod
@@ -7,3 +7,11 @@ require (
 	golang.org/x/exp v0.0.0-20230519143937-03e91628a987
 	golang.org/x/net v0.10.0
 )
+
+require (
+	github.com/Baozisoftware/qrcode-terminal-go v0.0.0-20170407111555-c0650d8dff0f // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+	golang.org/x/sys v0.8.0 // indirect
+)
--- internal/cmd/help.go
+++ internal/cmd/help.go
@@ -2,13 +2,17 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"net/url"
 	"os"
+
+	qrcodeTerminal "github.com/Baozisoftware/qrcode-terminal-go"
 )
 
 // printListenAddrs prints the addresses the server is listening on appending
-// the specified port to each one.
+// the specified port to each one.  It also prints a QR code for the first
+// found hostname.
 func printListenAddrs(port string) (err error) {
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
@@ -17,17 +21,12 @@ func printListenAddrs(port string) (err error) {
 		return fmt.Errorf("no interface addresses found")
 	}
 
-	fmt.Println("Listening on:")
-	hn, err := os.Hostname()
-	if err == nil {
-		fmt.Printf("\t%s\n", &url.URL{
-			Scheme: "http",
-			Host:   net.JoinHostPort(hn, port),
-		})
-	}
+	fmt.Println("Available at:")
 
 	for _, addr := range addrs {
 		if n, ok := addr.(*net.IPNet); !ok {
+			fmt.Printf("\t%s (not a net)\n", addr.String())
+
 			continue
 		} else {
 			fmt.Printf("\t%s\n", &url.URL{
@@ -36,6 +35,27 @@ func printListenAddrs(port string) (err error) {
 			})
 		}
 	}
+
+	hn, err := os.Hostname()
+	if err != nil {
+		// This error is not critical?
+		log.Printf("getting hostname: %s", err)
+
+		return nil
+	}
+
+	u := &url.URL{
+		Scheme: "http",
+		Host:   net.JoinHostPort(hn, port),
+	}
+
+	fmt.Println("Try also:")
+	qrcodeTerminal.New2(
+		qrcodeTerminal.ConsoleColors.BrightBlack,
+		qrcodeTerminal.ConsoleColors.BrightWhite,
+		qrcodeTerminal.QRCodeRecoveryLevels.Low,
+	).Get(u.String()).Print()
+	fmt.Printf("\t%s\n", u)
 
 	return nil
 }
